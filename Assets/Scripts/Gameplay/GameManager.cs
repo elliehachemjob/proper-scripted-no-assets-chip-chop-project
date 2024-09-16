7317008c02ac5d00ce3387e3b16185507bcd8a6a
ellie <elliehachemjob@gmail.com>
2024-09-16 15:34:28 +0200

    Gamemanager-part2

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -18,7 +18,42 @@ public class GameManager : MonoBehaviour
 	[SerializeField] private VoidEventChannelSO _cerisesMemoryEvent = default;
 	[SerializeField] private VoidEventChannelSO _decideOnDishesEvent = default;
 
+	private void Start()
+	{
+		StartGame();
+	}
 
+	private void OnEnable()
+	{
+		_addRockCandyRecipeEvent.OnEventRaised += AddRockCandyRecipe;
+		_cerisesMemoryEvent.OnEventRaised += AddSweetDoughRecipe;
+		_decideOnDishesEvent.OnEventRaised += AddFinalRecipes;
+	}
+
+	private void OnDisable()
+	{
+		_addRockCandyRecipeEvent.OnEventRaised -= AddRockCandyRecipe;
+		_cerisesMemoryEvent.OnEventRaised -= AddSweetDoughRecipe;
+		_decideOnDishesEvent.OnEventRaised -= AddFinalRecipes;
+	}
+
+	void AddRockCandyRecipe()
+	{
+		_inventory.Add(_rockCandyRecipe);
+	}
+
+	void AddSweetDoughRecipe()
+	{
+		_inventory.Add(_sweetDoughRecipe);
+	}
+
+	void AddFinalRecipes()
+	{
+		foreach (ItemSO item in _finalRecipes)
+		{
+			_inventory.Add(item);
+		}
+	}
 /* [SerializeField] private QuestManagerSO _questManager = default;
 	[SerializeField] private GameStateSO _gameState = default;
 
