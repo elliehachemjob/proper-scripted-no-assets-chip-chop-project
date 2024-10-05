9cb98b896bcb3f2f485f638e891a27e0c1ee4f7b
ellie <elliehachemjob@gmail.com>
2024-10-05 11:40:04 +0200

    filemanager

--- Assets/Scripts/SaveSystem/FileManager.cs
+++ Assets/Scripts/SaveSystem/FileManager.cs
@@ -5,6 +5,41 @@ using UnityEngine;
 public static class FileManager
 {
 	public static bool WriteToFile(string fileName, string fileContents)
+
+		var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+
+		try
+		{
+			File.WriteAllText(fullPath, fileContents);
+			return true;
+		}
+		catch (Exception e)
+		{
+			Debug.LogError($"Failed to write to {fullPath} with exception {e}");
+			return false;
+		}
+	}
+
+	public static bool LoadFromFile(string fileName, out string result)
+	{
+		var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+		if(!File.Exists(fullPath))
+		{
+			File.WriteAllText(fullPath, ""); 
+		}
+		try
+		{
+			result = File.ReadAllText(fullPath);
+			return true;
+		}
+		catch (Exception e)
+		{
+			Debug.LogError($"Failed to read from {fullPath} with exception {e}");
+			result = "";
+			return false;
+		}
+	}
+	/*
 	{
 		var fullPath = Path.Combine(Application.persistentDataPath, fileName);
 
@@ -66,5 +101,5 @@ public static class FileManager
 		}
 
 		return true;
-	}
+	}*/
 }
