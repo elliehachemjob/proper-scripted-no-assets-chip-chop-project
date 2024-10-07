b29db919205fc089faebb54c4cc8e066fc3bd518
ellie <elliehachemjob@gmail.com>
2024-10-07 22:21:48 +0200

    save-code

--- Assets/Scripts/SaveSystem/Save.cs
+++ Assets/Scripts/SaveSystem/Save.cs
@@ -10,6 +10,21 @@ using UnityEngine.Localization;
 [Serializable]
 public class Save
 {
+
+public string _locationId;
+	public List<SerializedItemStack> _itemStacks = new List<SerializedItemStack>();
+	public List<string> _finishedQuestlineItemsGUIds = new List<string>();
+
+	public float _masterVolume = default;
+	public float _musicVolume = default;
+	public float _sfxVolume = default;
+	public int _resolutionsIndex = default;
+	public int _antiAliasingIndex = default;
+	public float _shadowDistance = default;
+	public bool _isFullscreen = default;
+	public Locale _currentLocale = default;
+
+	/*
 	// This is test data, written according to TestScript.cs class
 	// This will change according to whatever data that needs to be stored
 
@@ -46,5 +61,5 @@ public class Save
 	public void LoadFromJson(string json)
 	{
 		JsonUtility.FromJsonOverwrite(json, this);
-	}
+	}*/
 }
