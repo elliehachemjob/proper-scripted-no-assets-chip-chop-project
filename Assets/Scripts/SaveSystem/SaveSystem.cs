68279381fd59f67229b40b3a0e0926c6f5efcf6d
ellie <elliehachemjob@gmail.com>
2024-10-11 11:02:36 +0200

    final-savesystem-code

--- Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -51,6 +51,68 @@ public class SaveSystem : ScriptableObject
 		return false;
 	}
 
+public IEnumerator LoadSavedInventory()
+	{
+		_playerInventory.Items.Clear();
+		foreach (var serializedItemStack in saveData._itemStacks)
+		{
+			var loadItemOperationHandle = Addressables.LoadAssetAsync<ItemSO>(serializedItemStack.itemGuid);
+			yield return loadItemOperationHandle;
+			if (loadItemOperationHandle.Status == AsyncOperationStatus.Succeeded)
+			{
+				var itemSO = loadItemOperationHandle.Result;
+				_playerInventory.Add(itemSO, serializedItemStack.amount);
+			}
+		}
+	}
+	public void LoadSavedQuestlineStatus()
+	{
+		_questManagerSO.SetFinishedQuestlineItemsFromSave(saveData._finishedQuestlineItemsGUIds);
+
+	}
+
+	public void SaveDataToDisk()
+	{
+		saveData._itemStacks.Clear();
+		foreach (var itemStack in _playerInventory.Items)
+		{
+			saveData._itemStacks.Add(new SerializedItemStack(itemStack.Item.Guid, itemStack.Amount));
+		}
+		saveData._finishedQuestlineItemsGUIds.Clear();
+
+		foreach (var item in _questManagerSO.GetFinishedQuestlineItemsGUIds())
+		{
+			saveData._finishedQuestlineItemsGUIds.Add(item);
+
+		}
+		if (FileManager.MoveFile(saveFilename, backupSaveFilename))
+		{
+			if (FileManager.WriteToFile(saveFilename, saveData.ToJson()))
+			{
+				//Debug.Log("Save successful " + saveFilename);
+			}
+		}
+	}
+
+	public void WriteEmptySaveFile()
+	{
+		FileManager.WriteToFile(saveFilename, "");
+
+	}
+	public void SetNewGameData()
+	{
+		FileManager.WriteToFile(saveFilename, "");
+		_playerInventory.Init();
+		_questManagerSO.ResetQuestlines();
+
+		SaveDataToDisk();
+
+	}
+	void SaveSettings()
+	{
+		saveData.SaveSettings(_currentSettings);
+
+	}
 
 	/*( [SerializeField] private VoidEventChannelSO _saveSettingsEvent = default;
 	[SerializeField] private LoadEventChannelSO _loadLocation = default;
