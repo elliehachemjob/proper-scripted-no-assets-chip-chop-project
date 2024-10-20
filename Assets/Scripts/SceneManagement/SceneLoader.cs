88bf133e561077e8281043bd36f6e58d3fbae0fa
ellie <elliehachemjob@gmail.com>
2024-10-20 14:50:55 +0200

    scene-loader-another-code-block

--- Assets/Scripts/SceneManagement/SceneLoader.cs
+++ Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -73,6 +73,109 @@ public class SceneLoader : MonoBehaviour
 			StartGameplay();
 		}
 	}
+
+	#endif
+
+	/// <summary>
+	/// This function loads the location scenes passed as array parameter
+	/// </summary>
+	private void LoadLocation(GameSceneSO locationToLoad, bool showLoadingScreen, bool fadeScreen)
+	{
+		//Prevent a double-loading, for situations where the player falls in two Exit colliders in one frame
+		if (_isLoading)
+			return;
+
+		_sceneToLoad = locationToLoad;
+		_showLoadingScreen = showLoadingScreen;
+		_isLoading = true;
+
+		//In case we are coming from the main menu, we need to load the Gameplay manager scene first
+		if (_gameplayManagerSceneInstance.Scene == null
+			|| !_gameplayManagerSceneInstance.Scene.isLoaded)
+		{
+			_gameplayManagerLoadingOpHandle = _gameplayScene.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);
+			_gameplayManagerLoadingOpHandle.Completed += OnGameplayManagersLoaded;
+		}
+		else
+		{
+			StartCoroutine(UnloadPreviousScene());
+		}
+	}
+
+	private void OnGameplayManagersLoaded(AsyncOperationHandle<SceneInstance> obj)
+	{
+		_gameplayManagerSceneInstance = _gameplayManagerLoadingOpHandle.Result;
+
+		StartCoroutine(UnloadPreviousScene());
+	}
+
+	/// <summary>
+	/// Prepares to load the main menu scene, first removing the Gameplay scene in case the game is coming back from gameplay to menus.
+	/// </summary>
+	private void LoadMenu(GameSceneSO menuToLoad, bool showLoadingScreen, bool fadeScreen)
+	{
+		//Prevent a double-loading, for situations where the player falls in two Exit colliders in one frame
+		if (_isLoading)
+			return;
+
+		_sceneToLoad = menuToLoad;
+		_showLoadingScreen = showLoadingScreen;
+		_isLoading = true;
+
+		//In case we are coming from a Location back to the main menu, we need to get rid of the persistent Gameplay manager scene
+		if (_gameplayManagerSceneInstance.Scene != null
+			&& _gameplayManagerSceneInstance.Scene.isLoaded)
+			Addressables.UnloadSceneAsync(_gameplayManagerLoadingOpHandle, true);
+
+		StartCoroutine(UnloadPreviousScene());
+	}
+
+	/// <summary>
+	/// In both Location and Menu loading, this function takes care of removing previously loaded scenes.
+	/// </summary>
+	private IEnumerator UnloadPreviousScene()
+	{
+		_inputReader.DisableAllInput();
+		_fadeRequestChannel.FadeOut(_fadeDuration);
+
+		yield return new WaitForSeconds(_fadeDuration);
+
+		if (_currentlyLoadedScene != null) //would be null if the game was started in Initialisation
+		{
+			if (_currentlyLoadedScene.sceneReference.OperationHandle.IsValid())
+			{
+				//Unload the scene through its AssetReference, i.e. through the Addressable system
+				_currentlyLoadedScene.sceneReference.UnLoadScene();
+			}
+#if UNITY_EDITOR
+			else
+			{
+				//Only used when, after a "cold start", the player moves to a new scene
+				//Since the AsyncOperationHandle has not been used (the scene was already open in the editor),
+				//the scene needs to be unloaded using regular SceneManager instead of as an Addressable
+				SceneManager.UnloadSceneAsync(_currentlyLoadedScene.sceneReference.editorAsset.name);
+			}
+#endif
+		}
+
+		LoadNewScene();
+	}
+
+	/// <summary>
+	/// Kicks off the asynchronous loading of a scene, either menu or Location.
+	/// </summary>
+	private void LoadNewScene()
+	{
+		if (_showLoadingScreen)
+		{
+			_toggleLoadingScreen.RaiseEvent(true);
+		}
+
+		_loadingOperationHandle = _sceneToLoad.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true, 0);
+		_loadingOperationHandle.Completed += OnNewSceneLoaded;
+	}
+
+
 /* 	[SerializeField] private GameSceneSO _gameplayScene = default;
 	[SerializeField] private InputReader _inputReader = default;
 
