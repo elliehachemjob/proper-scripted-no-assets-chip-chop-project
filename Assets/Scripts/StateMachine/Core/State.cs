7383c25f8ab615996f7e0a137f537770dbd3a5e6
ellie <elliehachemjob@gmail.com>
2024-10-26 14:23:14 +0200

    state-final-code

--- Assets/Scripts/StateMachine/Core/State.cs
+++ Assets/Scripts/StateMachine/Core/State.cs
@@ -33,7 +33,37 @@ namespace UOP1.StateMachine
 			OnStateEnter(_transitions);
 			OnStateEnter(_actions);
 		}
+		public void OnUpdate()
+		{
+			for (int i = 0; i < _actions.Length; i++)
+				_actions[i].OnUpdate();
+		}
 
+		public void OnStateExit()
+		{
+			void OnStateExit(IStateComponent[] comps)
+			{
+				for (int i = 0; i < comps.Length; i++)
+					comps[i].OnStateExit();
+			}
+			OnStateExit(_transitions);
+			OnStateExit(_actions);
+		}
+
+		public bool TryGetTransition(out State state)
+		{
+			state = null;
+
+			for (int i = 0; i < _transitions.Length; i++)
+				if (_transitions[i].TryGetTransiton(out state))
+					break;
+
+			for (int i = 0; i < _transitions.Length; i++)
+				_transitions[i].ClearConditionsCache();
+
+			return state != null;
+		}
+	}
 
 		/*internal StateSO _originSO;
 		internal StateMachine _stateMachine;
