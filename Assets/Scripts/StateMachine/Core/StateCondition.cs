a85c36825ff0642fc31ed9ef8a977587cfcca6f8
ellie <elliehachemjob@gmail.com>
2024-10-27 13:36:02 +0200

    state-condition

--- Assets/Scripts/StateMachine/Core/StateCondition.cs
+++ Assets/Scripts/StateMachine/Core/StateCondition.cs
@@ -2,7 +2,46 @@
 
 namespace UOP1.StateMachine
 {
+	
 	/// <summary>
+	/// Class that represents a conditional statement.
+	/// </summary>
+	public abstract class Condition : IStateComponent
+	{
+		private bool _isCached = false;
+		private bool _cachedStatement = default;
+		internal StateConditionSO _originSO;
+
+		/// <summary>
+		/// Use this property to access shared data from the <see cref="StateConditionSO"/> that corresponds to this <see cref="Condition"/>
+		/// </summary>
+		protected StateConditionSO OriginSO => _originSO;
+
+		/// <summary>
+		/// Specify the statement to evaluate.
+		/// </summary>
+		/// <returns></returns>
+		protected abstract bool Statement();
+
+		/// <summary>
+		/// Wrap the <see cref="Statement"/> so it can be cached.
+		/// </summary>
+		internal bool GetStatement()
+		{
+			if (!_isCached)
+			{
+				_isCached = true;
+				_cachedStatement = Statement();
+			}
+
+			return _cachedStatement;
+		}
+
+		internal void ClearStatementCache()
+		{
+			_isCached = false;
+		}
+	/* /// <summary>
 	/// Class that represents a conditional statement.
 	/// </summary>
 	public abstract class Condition : IStateComponent
@@ -76,5 +115,5 @@ namespace UOP1.StateMachine
 #endif
 			return isMet;
 		}
-	}
+	}*/
 }
