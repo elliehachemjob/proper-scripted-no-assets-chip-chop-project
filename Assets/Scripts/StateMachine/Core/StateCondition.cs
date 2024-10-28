ff7184015715c85c1f004bddf4df1d9d1a8ce618
ellie <elliehachemjob@gmail.com>
2024-10-28 11:38:02 +0200

    state-condition-latest

--- Assets/Scripts/StateMachine/Core/StateCondition.cs
+++ Assets/Scripts/StateMachine/Core/StateCondition.cs
@@ -41,6 +41,42 @@ namespace UOP1.StateMachine
 		{
 			_isCached = false;
 		}
+			/// <summary>
+		/// Awake is called when creating a new instance. Use this method to cache the components needed for the condition.
+		/// </summary>
+		/// <param name="stateMachine">The <see cref="StateMachine"/> this instance belongs to.</param>
+		public virtual void Awake(StateMachine stateMachine) { }
+		public virtual void OnStateEnter() { }
+		public virtual void OnStateExit() { }
+	}
+
+	/// <summary>
+	/// Struct containing a Condition and its expected result.
+	/// </summary>
+	public readonly struct StateCondition
+	{
+		internal readonly StateMachine _stateMachine;
+		internal readonly Condition _condition;
+		internal readonly bool _expectedResult;
+
+		public StateCondition(StateMachine stateMachine, Condition condition, bool expectedResult)
+		{
+			_stateMachine = stateMachine;
+			_condition = condition;
+			_expectedResult = expectedResult;
+		}
+
+		public bool IsMet()
+		{
+			bool statement = _condition.GetStatement();
+			bool isMet = statement == _expectedResult;
+
+#if UNITY_EDITOR
+			_stateMachine._debugger.TransitionConditionResult(_condition._originSO.name, statement, isMet);
+#endif
+			return isMet;
+		}
+	}
 	/* /// <summary>
 	/// Class that represents a conditional statement.
 	/// </summary>
