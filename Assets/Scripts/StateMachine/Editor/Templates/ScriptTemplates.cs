4e0f3c11ecb5384d97e65764472781a4632e44f2
ellie <elliehachemjob@gmail.com>
2024-11-04 10:05:18 +0200

    script-template-second-code

--- Assets/Scripts/StateMachine/Editor/Templates/ScriptTemplates.cs
+++ Assets/Scripts/StateMachine/Editor/Templates/ScriptTemplates.cs
@@ -24,7 +24,41 @@ internal class ScriptTemplates
 			"NewConditionSO.cs",
 			(Texture2D)EditorGUIUtility.IconContent("cs Script Icon").image,
 			$"{_path}/StateCondition.txt");
+private class DoCreateStateMachineScriptAsset : EndNameEditAction
+	{
+		public override void Action(int instanceId, string pathName, string resourceFile)
+		{
+			string text = File.ReadAllText(resourceFile);
 
+			string fileName = Path.GetFileName(pathName);
+			{
+				string newName = fileName.Replace(" ", "");
+				if (!newName.Contains("SO"))
+					newName = newName.Insert(fileName.Length - 3, "SO");
+
+				pathName = pathName.Replace(fileName, newName);
+				fileName = newName;
+			}
+
+			string fileNameWithoutExtension = fileName.Substring(0, fileName.Length - 3);
+			text = text.Replace("#SCRIPTNAME#", fileNameWithoutExtension);
+
+			string runtimeName = fileNameWithoutExtension.Replace("SO", "");
+			text = text.Replace("#RUNTIMENAME#", runtimeName);
+
+			for (int i = runtimeName.Length - 1; i > 0; i--)
+				if (char.IsUpper(runtimeName[i]) && char.IsLower(runtimeName[i - 1]))
+					runtimeName = runtimeName.Insert(i, " ");
+
+			text = text.Replace("#RUNTIMENAME_WITH_SPACES#", runtimeName);
+
+			string fullPath = Path.GetFullPath(pathName);
+			var encoding = new UTF8Encoding(true);
+			File.WriteAllText(fullPath, text, encoding);
+			AssetDatabase.ImportAsset(pathName);
+			ProjectWindowUtil.ShowCreatedAsset(AssetDatabase.LoadAssetAtPath(pathName, typeof(UnityEngine.Object)));
+		}
+	}
 /*
 	private static readonly string _path = "Assets/Scripts/StateMachine/Editor/Templates";
 
