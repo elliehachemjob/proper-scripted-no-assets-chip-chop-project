b627cb47f5bac34cfac7309c3aa858fa25f0fc8c
ellie <elliehachemjob@gmail.com>
2024-11-10 15:46:14 +0200

    init-only-attribute-drawer-part-1

--- Assets/Scripts/StateMachine/Editor/Utilities/InitOnlyAttributeDrawer.cs
+++ Assets/Scripts/StateMachine/Editor/Utilities/InitOnlyAttributeDrawer.cs
@@ -3,7 +3,26 @@ using UnityEngine;
 
 namespace UOP1.StateMachine.Editor
 {
-	[CustomPropertyDrawer(typeof(InitOnlyAttribute))]
+
+		[CustomPropertyDrawer(typeof(InitOnlyAttribute))]
+	public class InitOnlyAttributeDrawer : PropertyDrawer
+	{
+		private static readonly string _text = "Changes to this parameter during Play mode won't be reflected on existing StateMachines";
+		private static readonly GUIStyle _style = new GUIStyle(GUI.skin.GetStyle("helpbox")) { padding = new RectOffset(5, 5, 5, 5) };
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			if (EditorApplication.isPlaying)
+			{
+				position.height = _style.CalcHeight(new GUIContent(_text), EditorGUIUtility.currentViewWidth);
+				EditorGUI.HelpBox(position, _text, MessageType.Info);
+				position.y += position.height + EditorGUIUtility.standardVerticalSpacing;
+				position.height = EditorGUI.GetPropertyHeight(property, label);
+			}
+
+			EditorGUI.PropertyField(position, property, label);
+		}
+	/*[CustomPropertyDrawer(typeof(InitOnlyAttribute))]
 	public class InitOnlyAttributeDrawer : PropertyDrawer
 	{
 		private static readonly string _text = "Changes to this parameter during Play mode won't be reflected on existing StateMachines";
@@ -34,5 +53,5 @@ namespace UOP1.StateMachine.Editor
 
 			return height;
 		}
-	}
+	}*/
 }
