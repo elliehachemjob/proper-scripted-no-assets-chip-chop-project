e59f2116ff0da90cc4d6bbedf8371adbeb5b39c8
ellie <elliehachemjob@gmail.com>
2024-11-28 13:00:49 +0200

    State-Condition-So-Part-1

--- Assets/Scripts/StateMachine/ScriptableObjects/StateConditionSO.cs
+++ Assets/Scripts/StateMachine/ScriptableObjects/StateConditionSO.cs
@@ -5,6 +5,8 @@ namespace UOP1.StateMachine.ScriptableObjects
 {
 	public abstract class StateConditionSO : ScriptableObject
 	{
+
+		
 		/// <summary>
 		/// Will create a new custom <see cref="Condition"/> or use an existing one inside <paramref name="createdInstances"/>.
 		/// </summary>
@@ -25,9 +27,26 @@ namespace UOP1.StateMachine.ScriptableObjects
 		protected abstract Condition CreateCondition();
 	}
 
+/* 		internal StateCondition GetCondition(StateMachine stateMachine, bool expectedResult, Dictionary<ScriptableObject, object> createdInstances)
+		{
+			if (!createdInstances.TryGetValue(this, out var obj))
+			{
+				var condition = CreateCondition();
+				condition._originSO = this;
+				createdInstances.Add(this, condition);
+				condition.Awake(stateMachine);
+
+				obj = condition;
+			}
+
+			return new StateCondition(stateMachine, (Condition)obj, expectedResult);
+		}
+		protected abstract Condition CreateCondition();
+	}
+
 
 	public abstract class StateConditionSO<T> : StateConditionSO where T : Condition, new()
 	{
 		protected override Condition CreateCondition() => new T();
-	}
+	}*/
 }
