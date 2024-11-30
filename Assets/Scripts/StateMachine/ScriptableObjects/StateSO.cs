3fea10d55f0bced61ccad12fe35f95c58f64090b
ellie <elliehachemjob@gmail.com>
2024-11-30 16:24:11 +0200

    State-SO-Finishing-first-part

--- Assets/Scripts/StateMachine/ScriptableObjects/StateSO.cs
+++ Assets/Scripts/StateMachine/ScriptableObjects/StateSO.cs
@@ -3,6 +3,29 @@ using UnityEngine;
 
 namespace UOP1.StateMachine.ScriptableObjects
 {
+	[CreateAssetMenu(fileName = "New State", menuName = "State Machines/State")]
+	public class StateSO : ScriptableObject
+	{
+		[SerializeField] private StateActionSO[] _actions = null;
+
+		/// <summary>
+		/// Will create a new state or return an existing one inside <paramref name="createdInstances"/>.
+		/// </summary>
+		internal State GetState(StateMachine stateMachine, Dictionary<ScriptableObject, object> createdInstances)
+		{
+			if (createdInstances.TryGetValue(this, out var obj))
+				return (State)obj;
+
+			var state = new State();
+			createdInstances.Add(this, state);
+
+			state._originSO = this;
+			state._stateMachine = stateMachine;
+			state._transitions = new StateTransition[0];
+			state._actions = GetActions(_actions, stateMachine, createdInstances);
+
+			return state;
+		}
 	/*
 	[CreateAssetMenu(fileName = "New State", menuName = "State Machines/State")]
 	public class StateSO : ScriptableObject
