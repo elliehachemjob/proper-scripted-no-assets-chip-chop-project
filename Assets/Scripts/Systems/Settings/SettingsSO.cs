897e32e3617783d2992c405eecb7df80230edb38
ellie <elliehachemjob@gmail.com>
2024-12-05 11:36:39 +0200

    Setting-SO-Part-2

--- Assets/Scripts/Systems/Settings/SettingsSO.cs
+++ Assets/Scripts/Systems/Settings/SettingsSO.cs
@@ -20,6 +20,36 @@ public class SettingsSO : ScriptableObject
 	public float ShadowDistance => _shadowDistance;
 	public bool IsFullscreen => _isFullscreen;
 	public Locale CurrentLocale => _currentLocale;
+
+		public void SaveAudioSettings(float newMusicVolume, float newSfxVolume, float newMasterVolume)
+	{
+		_masterVolume = newMasterVolume;
+		_musicVolume = newMusicVolume;
+		_sfxVolume = newSfxVolume;
+	}
+	public void SaveGraphicsSettings(int newResolutionsIndex, int newAntiAliasingIndex, float newShadowDistance, bool fullscreenState)
+	{
+		_resolutionsIndex = newResolutionsIndex;
+		_antiAliasingIndex = newAntiAliasingIndex;
+		_shadowDistance = newShadowDistance;
+		_isFullscreen = fullscreenState;
+	}
+	public void SaveLanguageSettings(Locale local)
+	{
+		_currentLocale = local;
+	}
+	public SettingsSO() { }
+	public void LoadSavedSettings(Save savedFile)
+	{
+		_masterVolume = savedFile._masterVolume;
+		_musicVolume = savedFile._musicVolume;
+		_sfxVolume = savedFile._sfxVolume;
+		_resolutionsIndex = savedFile._resolutionsIndex;
+		_antiAliasingIndex = savedFile._antiAliasingIndex;
+		_shadowDistance = savedFile._shadowDistance;
+		_isFullscreen = savedFile._isFullscreen;
+		_currentLocale = savedFile._currentLocale;
+	}
 /* 	[SerializeField] float _masterVolume = default;
 	[SerializeField] float _musicVolume = default;
 	[SerializeField] float _sfxVolume = default;
