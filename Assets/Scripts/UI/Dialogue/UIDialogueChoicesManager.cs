f50655e14265658d3964fbef2680e8967215f15b
ellie <elliehachemjob@gmail.com>
2024-12-25 10:33:55 +0200

    Half-block-code-UI-Dialogue-choices-manager

--- Assets/Scripts/UI/Dialogue/UIDialogueChoicesManager.cs
+++ Assets/Scripts/UI/Dialogue/UIDialogueChoicesManager.cs
@@ -3,7 +3,30 @@ using UnityEngine;
 
 public class UIDialogueChoicesManager : MonoBehaviour
 {
-	[SerializeField] private UIDialogueChoiceFiller[] _choiceButtons;
+
+		[SerializeField] private UIDialogueChoiceFiller[] _choiceButtons;
+
+	public void FillChoices(List<Choice> choices)
+	{
+		if (choices != null)
+		{
+			int maxCount = Mathf.Max(choices.Count, _choiceButtons.Length);
+
+			for (int i = 0; i < maxCount; i++)
+			{
+				if (i < _choiceButtons.Length)
+				{
+					if (i < choices.Count)
+					{
+						_choiceButtons[i].gameObject.SetActive(true);
+						_choiceButtons[i].FillChoice(choices[i], i == 0);
+					}
+					else
+					{
+						_choiceButtons[i].gameObject.SetActive(false);
+					}
+	
+	/* [SerializeField] private UIDialogueChoiceFiller[] _choiceButtons;
 
 	public void FillChoices(List<Choice> choices)
 	{
@@ -31,6 +54,6 @@ public class UIDialogueChoicesManager : MonoBehaviour
 				}
 			}
 		}
-	}
+	}/* 
 }
 
