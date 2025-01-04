d1931321cef5ade77df7f70b056d53f728e0e9c2
ellie <elliehachemjob@gmail.com>
2025-01-04 19:12:19 +0200

    UI-Inspector-ingredient

--- Assets/Scripts/UI/Inventory/UIInspectorIngredients.cs
+++ Assets/Scripts/UI/Inventory/UIInspectorIngredients.cs
@@ -9,6 +9,31 @@ public class UIInspectorIngredients : MonoBehaviour
 	{
 
 		int maxCount = Mathf.Max(listofIngredients.Count, _instantiatedGameObjects.Count);
+
+		
+		for (int i = 0; i < maxCount; i++)
+		{
+			if (i < listofIngredients.Count)
+			{
+				if (i >= _instantiatedGameObjects.Count)
+				{
+					//Do nothing, maximum ingredients for a recipe reached
+					Debug.Log("Maximum ingredients reached");
+				}
+				else
+				{
+					bool isAvailable = availabilityArray[i];
+					_instantiatedGameObjects[i].FillIngredient(listofIngredients[i], isAvailable);
+
+					_instantiatedGameObjects[i].gameObject.SetActive(true);
+				}
+			}
+			else if (i < _instantiatedGameObjects.Count)
+			{
+				_instantiatedGameObjects[i].gameObject.SetActive(false);
+			}
+		}
+	}
 	/* [SerializeField] private List<UIInspectorIngredientFiller> _instantiatedGameObjects = new List<UIInspectorIngredientFiller>();
 
 	public void FillIngredients(List<ItemStack> listofIngredients, bool[] availabilityArray)
