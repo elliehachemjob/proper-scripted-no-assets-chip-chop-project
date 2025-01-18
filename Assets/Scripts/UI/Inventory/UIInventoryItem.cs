d3cd438bb08166a71b410e84053c1a9b7e48fa0f
ellie <elliehachemjob@gmail.com>
2025-01-18 12:21:42 +0200

    UI-Inventory-item

--- Assets/Scripts/UI/Inventory/UIInventoryItem.cs
+++ Assets/Scripts/UI/Inventory/UIInventoryItem.cs
@@ -102,7 +102,11 @@ public class UIInventoryItem : MonoBehaviour
 			_imgSelected.gameObject.SetActive(false);
 		}
 	}
-
+	public void UnselectItem()
+	{
+		_isSelected = false;
+		_imgSelected.gameObject.SetActive(false);
+	}
 
 	/* [SerializeField] private TextMeshProUGUI _itemCount = default;
 	[SerializeField] private Image _itemPreviewImage = default;
