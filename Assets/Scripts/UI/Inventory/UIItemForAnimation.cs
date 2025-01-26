d5fd6cd7a754455210c110ebb28ed7ddfc627108
ellie <elliehachemjob@gmail.com>
2025-01-26 15:10:02 +0200

    UI-Item-For-animation

--- Assets/Scripts/UI/Inventory/UIItemForAnimation.cs
+++ Assets/Scripts/UI/Inventory/UIItemForAnimation.cs
@@ -11,6 +11,25 @@ public class UIItemForAnimation : MonoBehaviour
 
 	public event UnityAction AnimationEnded;
 
+	public void SetItem(ItemSO item)
+	{
+		if (item.IsLocalized)
+		{
+			_bgLocalizedImage.enabled = true;
+			_bgLocalizedImage.AssetReference = item.LocalizePreviewImage;
+		}
+		else
+		{
+			_bgLocalizedImage.enabled = false;
+			_itemPreviewImage.sprite = item.PreviewImage;
+		}
+		_bgImage.color = item.ItemType.TypeColor;
+	}
+
+	public void OnAnimationEnded()
+	{
+		AnimationEnded.Invoke();
+	}
 /* [SerializeField] private LocalizeSpriteEvent _bgLocalizedImage = default;
 	[SerializeField] private Image _itemPreviewImage = default;
 	[SerializeField] private Image _bgImage = default;
