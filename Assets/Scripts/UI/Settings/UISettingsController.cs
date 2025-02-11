b28662f77e6fcf0d02c493ba117db858de7a8b1f
ellie <elliehachemjob@gmail.com>
2025-02-11 15:22:54 +0200

    UI-Settings-Controller

--- Assets/Scripts/UI/Settings/UISettingsController.cs
+++ Assets/Scripts/UI/Settings/UISettingsController.cs
@@ -79,6 +79,70 @@ public class UISettingsController : MonoBehaviour
 	{
 		Closed.Invoke();
 	}
+	void OpenSetting(SettingsType settingType)
+	{
+		_selectedTab = settingType;
+		switch (settingType)
+		{
+			case SettingsType.Language:
+				_currentSettings.SaveLanguageSettings(_currentSettings.CurrentLocale);
+				break;
+			case SettingsType.Graphics:
+				_graphicsComponent.Setup();
+				break;
+			case SettingsType.Audio:
+				_audioComponent.Setup(_currentSettings.MusicVolume, _currentSettings.SfxVolume, _currentSettings.MasterVolume);
+				break;
+			default:
+				break;
+		}
+
+		_languageComponent.gameObject.SetActive(settingType == SettingsType.Language);
+		_graphicsComponent.gameObject.SetActive((settingType == SettingsType.Graphics));
+		_audioComponent.gameObject.SetActive(settingType == SettingsType.Audio);
+		_settingTabFiller.SelectTab(settingType);
+
+	}
+	void SwitchTab(float orientation)
+	{
+
+		if (orientation != 0)
+		{
+			bool isLeft = orientation < 0;
+			int initialIndex = _settingTabsList.FindIndex(o => o == _selectedTab);
+			if (initialIndex != -1)
+			{
+				if (isLeft)
+				{
+					initialIndex--;
+				}
+				else
+				{
+					initialIndex++;
+				}
+
+				initialIndex = Mathf.Clamp(initialIndex, 0, _settingTabsList.Count - 1);
+			}
+
+			OpenSetting(_settingTabsList[initialIndex]);
+		}
+	}
+	public void SaveLaguageSettings(Locale local)
+	{
+		_currentSettings.SaveLanguageSettings(local);
+		SaveSettingsEvent.RaiseEvent();
+	}
+	public void SaveGraphicsSettings(int newResolutionsIndex, int newAntiAliasingIndex, float newShadowDistance, bool fullscreenState)
+	{
+		_currentSettings.SaveGraphicsSettings(newResolutionsIndex, newAntiAliasingIndex, newShadowDistance, fullscreenState);
+		SaveSettingsEvent.RaiseEvent();
+	}
+	void SaveAudioSettings(float musicVolume, float sfxVolume, float masterVolume)
+	{
+		_currentSettings.SaveAudioSettings(musicVolume, sfxVolume, masterVolume);
+
+		SaveSettingsEvent.RaiseEvent();
+	}
 
 	/*Language,
 	Volume_SFx,
