ff77036628d100530473a2b4a74f94f92e38b92b
ellie <elliehachemjob@gmail.com>
2025-02-15 19:38:37 +0200

    UI-Settings-Tabs-Filler-code-block

--- Assets/Scripts/UI/Settings/UISettingTabsFiller.cs
+++ Assets/Scripts/UI/Settings/UISettingTabsFiller.cs
@@ -24,6 +24,25 @@ public class UISettingTabsFiller : MonoBehaviour
 			_settingTabsList[i].Clicked -= ChangeTab;
 		}
 	}
+/* public UnityAction<SettingsType> ChooseTab;
+	[SerializeField]
+	private UISettingTabFiller[] _settingTabsList = default;
+	public void FillTabs(List<SettingsType> settingTabs)
+	{
+		for (int i = 0; i < settingTabs.Count; i++)
+		{
+			_settingTabsList[i].SetTab(settingTabs[i], i == 0);
+			_settingTabsList[i].Clicked += ChangeTab;
+		}
+
+	}
+	private void OnDisable()
+	{
+		for (int i = 0; i < _settingTabsList.Length; i++)
+		{
+			_settingTabsList[i].Clicked -= ChangeTab;
+		}
+	}
 	public void SelectTab(SettingsType tabType)
 	{
 		for (int i = 0; i < _settingTabsList.Length; i++)
@@ -35,5 +54,5 @@ public class UISettingTabsFiller : MonoBehaviour
 	public void ChangeTab(SettingsType tabType)
 	{
 		ChooseTab.Invoke(tabType);
-	}
+	}*/
 }
