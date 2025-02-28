e67c46f2d4d8ccfca5f7b8ae1cfd635da3b922a8
ellie <elliehachemjob@gmail.com>
2025-02-28 14:55:39 +0200

    UI-Health-Bar-Manager

--- Assets/Scripts/UI/UIHealthBarManager.cs
+++ Assets/Scripts/UI/UIHealthBarManager.cs
@@ -21,6 +21,38 @@ public class UIHealthBarManager : MonoBehaviour
 		_UIUpdateNeeded.OnEventRaised -= UpdateHeartImages;
 	}
 
+	private void InitializeHealthBar()
+	{
+		_protagonistHealth.SetMaxHealth(_healthConfig.InitialHealth);
+		_protagonistHealth.SetCurrentHealth(_healthConfig.InitialHealth);
+
+		UpdateHeartImages();
+	}
+
+	private void UpdateHeartImages()
+	{
+		int heartValue = _protagonistHealth.MaxHealth / _heartImages.Length;
+		int filledHeartCount = Mathf.FloorToInt((float)_protagonistHealth.CurrentHealth / heartValue);
+
+		for (int i = 0; i < _heartImages.Length; i++)
+		{
+			float heartPercent = 0;
+
+			if (i < filledHeartCount)
+			{
+				heartPercent = 1;
+			}
+			else if (i == filledHeartCount)
+			{
+				heartPercent = ((float)_protagonistHealth.CurrentHealth - (float)filledHeartCount * (float)heartValue) / (float)heartValue;
+			}
+			else
+			{
+				heartPercent = 0;
+			}
+			_heartImages[i].SetImage(heartPercent);
+		}
+	}
 	/* [SerializeField] private HealthSO _protagonistHealth = default; //the HealthBar is watching this object, which is the health of the player
 	[SerializeField] private HealthConfigSO _healthConfig = default;
 	[SerializeField] private UIHeartDisplay[] _heartImages = default;
