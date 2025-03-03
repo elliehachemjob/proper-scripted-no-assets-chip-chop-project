7a2e5e944a043ac632838b680f0b317889ad417e
ellie <elliehachemjob@gmail.com>
2025-03-03 14:26:19 +0200

    UI-Interaction

--- Assets/Scripts/UI/UIInteraction.cs
+++ Assets/Scripts/UI/UIInteraction.cs
@@ -16,4 +16,15 @@ public class UIInteraction : MonoBehaviour
 			_interactionIcon.sprite = icon;
 		}
 	}
-}
+/*	[SerializeField] private List<InteractionSO> _listInteractions = default;
+	[SerializeField] Image _interactionIcon = default;
+
+	public void FillInteractionPanel(InteractionType interactionType)
+	{
+		if (_listInteractions != null
+			&& _listInteractions.Exists(o => o.InteractionType == interactionType))
+		{
+			Sprite icon = (_listInteractions.Find(o => o.InteractionType == interactionType)).InteractionIcon;
+			_interactionIcon.sprite = icon;
+		}
+	}*/
