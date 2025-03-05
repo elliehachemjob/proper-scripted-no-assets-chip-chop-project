fafb4118f915979136a43f077ab2ae79d448ab7e
ellie <elliehachemjob@gmail.com>
2025-03-05 13:25:53 +0200

    UI-Main-Menu

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -12,6 +12,34 @@ public class UIMainMenu : MonoBehaviour
 	public UnityAction SettingsButtonAction;
 	public UnityAction CreditsButtonAction;
 	public UnityAction ExitButtonAction;
+	public void SetMenuScreen(bool hasSaveData)
+	{
+		_continueButton.interactable = hasSaveData;
+		if (hasSaveData)
+		{
+			_continueButton.Select();
+		}
+		else
+		{
+			_NewGameButton.Select();
+		}
+	}
+
+	public void NewGameButton()
+	{
+		NewGameButtonAction.Invoke();
+	}
+
+	public void ContinueButton()
+	{
+		ContinueButtonAction.Invoke();
+	}
+
+	public void SettingsButton()
+	{
+		SettingsButtonAction.Invoke();
+	}
+
 /* 	[SerializeField] private Button _continueButton = default;
 	[SerializeField] private Button _NewGameButton = default;
 
