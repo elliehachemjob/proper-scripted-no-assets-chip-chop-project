2cb78e4ce4674bbaffe7535dd94177c06aafabc3
ellie <elliehachemjob@gmail.com>
2025-03-25 15:46:26 +0200

    UI-popup

--- Assets/Scripts/UI/UIPopup.cs
+++ Assets/Scripts/UI/UIPopup.cs
@@ -40,6 +40,54 @@ public class UIPopup : MonoBehaviour
     }
 
 }
+
+    public void SetPopup(PopupType popupType)
+    {
+        _actualType = popupType;
+        bool isConfirmation = false;
+        bool hasExitButton = false;
+        _titleText.StringReference.TableEntryReference = _actualType.ToString() + "_Popup_Title";
+        _descriptionText.StringReference.TableEntryReference = _actualType.ToString() + "_Popup_Description";
+        string tableEntryReferenceConfirm = PopupButtonType.Confirm + "_" + _actualType;
+        string tableEntryReferenceCancel = PopupButtonType.Cancel + "_" + _actualType;
+        switch (_actualType)
+        {
+            case PopupType.NewGame:
+            case PopupType.BackToMenu:
+                isConfirmation = true;
+
+                _popupButton1.SetButton(tableEntryReferenceConfirm, true);
+                _popupButton2.SetButton(tableEntryReferenceCancel, false);
+                hasExitButton = true;
+                break;
+            case PopupType.Quit:
+                isConfirmation = true;
+                _popupButton1.SetButton(tableEntryReferenceConfirm, true);
+                _popupButton2.SetButton(tableEntryReferenceCancel, false);
+                hasExitButton = false;
+                break;
+            default:
+                isConfirmation = false;
+                hasExitButton = false;
+                break;
+        }
+
+        if (isConfirmation) // needs two button : Is a decision 
+        {
+            _popupButton1.gameObject.SetActive(true);
+            _popupButton2.gameObject.SetActive(true);
+
+            _popupButton2.Clicked += CancelButtonClicked;
+            _popupButton1.Clicked += ConfirmButtonClicked;
+        }
+        else // needs only one button : Is an information 
+        {
+            _popupButton1.gameObject.SetActive(true);
+            _popupButton2.gameObject.SetActive(false);
+
+            _popupButton1.Clicked += ConfirmButtonClicked;
+        }
+
    /* [SerializeField] private LocalizeStringEvent _titleText = default;
     [SerializeField] private LocalizeStringEvent _descriptionText = default;
     [SerializeField] private Button _buttonClose = default;
