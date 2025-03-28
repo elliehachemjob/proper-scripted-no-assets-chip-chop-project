de8b69440dcbce38ffff4a29154919a214708dd3
ellie <elliehachemjob@gmail.com>
2025-03-28 15:23:51 +0200

    Animation-code-1

--- Assets/Scripts/Animation/RandomIdleAnimationBehaviour.cs
+++ Assets/Scripts/Animation/RandomIdleAnimationBehaviour.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class RandomIdleAnimationBehaviour : StateMachineBehaviour
 {
-	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
 		int randomIdle = Random.Range(0, 2);
 		animator.SetInteger("RandomIdle", randomIdle);
 	}
+/*	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		int randomIdle = Random.Range(0, 2);
+		animator.SetInteger("RandomIdle", randomIdle);
+	}*/
 }
