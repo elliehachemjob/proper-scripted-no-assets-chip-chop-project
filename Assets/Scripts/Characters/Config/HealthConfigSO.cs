bdd32d37addb49865c8e81b7a217bd0f2e406bba
ellie <elliehachemjob@gmail.com>
2025-04-01 15:39:47 +0200

    Health-config-So

--- Assets/Scripts/Characters/Config/HealthConfigSO.cs
+++ Assets/Scripts/Characters/Config/HealthConfigSO.cs
@@ -3,8 +3,9 @@
 [CreateAssetMenu(fileName = "HealthConfig", menuName = "EntityConfig/Health Config")]
 public class HealthConfigSO : ScriptableObject
 {
-	[SerializeField] private int _initialHealth;
+/*	[SerializeField] private int _initialHealth;
 
 	public int InitialHealth => _initialHealth;
 
+}*/
 }
