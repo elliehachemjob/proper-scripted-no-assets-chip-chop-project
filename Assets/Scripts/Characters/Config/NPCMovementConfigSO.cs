b4d2038d7602143aca156df172533d7a3f234229
ellie <elliehachemjob@gmail.com>
2025-04-02 11:46:14 +0200

    NPC-movements-configs-so

--- Assets/Scripts/Characters/Config/NPCMovementConfigSO.cs
+++ Assets/Scripts/Characters/Config/NPCMovementConfigSO.cs
@@ -2,9 +2,16 @@
 
 public class NPCMovementConfigSO : ScriptableObject
 {
-	[SerializeField] private float _stopDuration;
+		[SerializeField] private float _stopDuration;
 	[SerializeField] private float _speed;
 
 	public float Speed => _speed;
 	public float StopDuration => _stopDuration;
+09
+	/* 
+	[SerializeField] private float _stopDuration;
+	[SerializeField] private float _speed;
+
+	public float Speed => _speed;
+	public float StopDuration => _stopDuration;*/
 }
