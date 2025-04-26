41842e29bc957a17b55b3c4273f3d26d085bfa93
ellie <elliehachemjob@gmail.com>
2025-04-26 15:59:24 +0300

    Ascend-Action-SO

--- Assets/Scripts/Characters/StateMachine/Actions/AscendActionSO.cs
+++ Assets/Scripts/Characters/StateMachine/Actions/AscendActionSO.cs
@@ -22,6 +22,22 @@ public class AscendAction : StateAction
 	{
 		_protagonistScript = stateMachine.GetComponent<Protagonist>();
 	}
+		public override void OnStateEnter()
+	{
+		_verticalMovement = _originSO.initialJumpForce;
+	}
+
+	public override void OnUpdate()
+	{
+		_gravityContributionMultiplier += Protagonist.GRAVITY_COMEBACK_MULTIPLIER;
+		_gravityContributionMultiplier *= Protagonist.GRAVITY_DIVIDER; //Reduce the gravity effect
+
+		//Note that deltaTime is used even though it's going to be used in ApplyMovementVectorAction, this is because it represents an acceleration, not a speed
+		_verticalMovement += Physics.gravity.y * Protagonist.GRAVITY_MULTIPLIER * _gravityContributionMultiplier * Time.deltaTime;
+		//Note that even if it's added, the above value is negative due to Physics.gravity.y
+
+		_protagonistScript.movementVector.y = _verticalMovement;
+	}
 }
 /*	[Tooltip("The initial upwards push when pressing jump. This is injected into verticalMovement, and gradually cancelled by gravity")]
 	public float initialJumpForce = 6f;
