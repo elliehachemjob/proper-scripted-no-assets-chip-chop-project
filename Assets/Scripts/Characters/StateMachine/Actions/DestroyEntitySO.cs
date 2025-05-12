f369f441d57448e7cf40c4dcf1752686a7331417
ellie <elliehachemjob@gmail.com>
2025-05-12 10:56:28 +0300

    Destroy-Entity-So

--- Assets/Scripts/Characters/StateMachine/Actions/DestroyEntitySO.cs
+++ Assets/Scripts/Characters/StateMachine/Actions/DestroyEntitySO.cs
@@ -4,6 +4,7 @@ using UOP1.StateMachine.ScriptableObjects;
 
 [CreateAssetMenu(fileName = "DestroyEntity", menuName = "State Machines/Actions/Destroy Entity")]
 public class DestroyEntitySO : StateActionSO
+public class DestroyEntitySO : StateActionSO
 {
 	protected override StateAction CreateAction() => new DestroyEntity();
 }
@@ -25,5 +26,27 @@ public class DestroyEntity : StateAction
 	public override void OnStateEnter()
 	{
 		GameObject.Destroy(_gameObject);
-	}
+	}}
+/*{
+	protected override StateAction CreateAction() => new DestroyEntity();
 }
+
+public class DestroyEntity : StateAction
+{
+	private GameObject _gameObject;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_gameObject = stateMachine.gameObject;
+	}
+
+	public override void OnUpdate()
+	{
+
+	}
+
+	public override void OnStateEnter()
+	{
+		GameObject.Destroy(_gameObject);
+	}
+}*/
