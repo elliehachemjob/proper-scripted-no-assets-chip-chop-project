a4b19845292a4a99f18f442c48711100ca3548fa
ellie <elliehachemjob@gmail.com>
2025-05-21 11:18:06 +0300

    GroundGravityAction-So

--- Assets/Scripts/Characters/StateMachine/Actions/GroundGravityActionSO.cs
+++ Assets/Scripts/Characters/StateMachine/Actions/GroundGravityActionSO.cs
@@ -9,8 +9,29 @@ public class GroundGravityActionSO : StateActionSO<GroundGravityAction>
 		Color finalTintingColor = Color.Lerp(_baseTintColor, _flashingColor, _flashingColor.a);
 		float tintingTiming = (_getHitFlashingDuration - _innerFlashingTime) * _getHitFlashingSpeed / _getHitFlashingDuration;
 		return Color.Lerp(_baseTintColor, finalTintingColor, (-Mathf.Cos(Mathf.PI * 2 * tintingTiming) + 1) / 2);
+
+		[Tooltip("Vertical movement pulling down the player to keep it anchored to the ground.")]
+	public float verticalPull = -5f;
+}
+
+public class GroundGravityAction : StateAction
+{
+	//Component references
+	private Protagonist _protagonistScript;
+
+	private GroundGravityActionSO _originSO => (GroundGravityActionSO)base.OriginSO; // The SO this StateAction spawned from
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_protagonistScript = stateMachine.GetComponent<Protagonist>();
+	}
 	}
 /*{
+
+Color finalTintingColor = Color.Lerp(_baseTintColor, _flashingColor, _flashingColor.a);
+		float tintingTiming = (_getHitFlashingDuration - _innerFlashingTime) * _getHitFlashingSpeed / _getHitFlashingDuration;
+		return Color.Lerp(_baseTintColor, finalTintingColor, (-Mathf.Cos(Mathf.PI * 2 * tintingTiming) + 1) / 2);
+
 	[Tooltip("Vertical movement pulling down the player to keep it anchored to the ground.")]
 	public float verticalPull = -5f;
 }
