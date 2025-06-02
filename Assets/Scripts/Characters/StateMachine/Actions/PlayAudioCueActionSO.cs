fde89c62b53bcaaca5609832c2f053414f51f5e6
ellie <elliehachemjob@gmail.com>
2025-06-02 10:17:29 +0300

    PlayAudioCueActionsSO

--- Assets/Scripts/Characters/StateMachine/Actions/PlayAudioCueActionSO.cs
+++ Assets/Scripts/Characters/StateMachine/Actions/PlayAudioCueActionSO.cs
@@ -6,7 +6,25 @@ using UOP1.StateMachine.ScriptableObjects;
 
 [CreateAssetMenu(menuName = "State Machines/Actions/Play AudioCue")]
 public class PlayAudioCueActionSO : StateActionSO<PlayAudioCueAction>
+
+public class PlayAudioCueActionSO : StateActionSO<PlayAudioCueAction>
 {
+	public AudioCueSO audioCue = default;
+	public AudioCueEventChannelSO audioCueEventChannel = default;
+	public AudioConfigurationSO audioConfiguration = default;
+}
+
+public class PlayAudioCueAction : StateAction
+{
+	private Transform _stateMachineTransform;
+
+	private PlayAudioCueActionSO _originSO => (PlayAudioCueActionSO)base.OriginSO; // The SO this StateAction spawned from
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_stateMachineTransform = stateMachine.transform;
+	}}
+/*{
 	public AudioCueSO audioCue = default;
 	public AudioCueEventChannelSO audioCueEventChannel = default;
 	public AudioConfigurationSO audioConfiguration = default;
@@ -29,4 +47,4 @@ public class PlayAudioCueAction : StateAction
 	{
 		_originSO.audioCueEventChannel.RaisePlayEvent(_originSO.audioCue, _originSO.audioConfiguration, _stateMachineTransform.position);
 	}
-}
+}*/
