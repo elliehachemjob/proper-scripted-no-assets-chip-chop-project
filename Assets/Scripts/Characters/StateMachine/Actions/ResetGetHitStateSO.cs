b1407e82d680c83963a7837959e6f67f4f167674
ellie <elliehachemjob@gmail.com>
2025-06-16 13:01:18 +0300

    Rest-Get-Hit-State-So

--- Assets/Scripts/Characters/StateMachine/Actions/ResetGetHitStateSO.cs
+++ Assets/Scripts/Characters/StateMachine/Actions/ResetGetHitStateSO.cs
@@ -4,9 +4,17 @@ using UOP1.StateMachine.ScriptableObjects;
 
 [CreateAssetMenu(fileName = "ResetGetHitState", menuName = "State Machines/Actions/Reset Get Hit State")]
 public class ResetGetHitStateSO : StateActionSO
-s{
+{
 	protected override StateAction CreateAction() => new ResetGetHitState();
 }
+public class ResetGetHitState : StateAction
+{
+	private Damageable _damageableEntity;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_damageableEntity = stateMachine.GetComponent<Damageable>();
+	}}
 /*{
 	protected override StateAction CreateAction() => new ResetGetHitState();
 }
