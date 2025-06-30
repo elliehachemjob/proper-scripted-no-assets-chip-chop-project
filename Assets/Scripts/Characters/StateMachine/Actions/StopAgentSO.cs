95d9ea3953e7e54f8128e6f0d8205a6d57264dc1
ellie <elliehachemjob@gmail.com>
2025-06-30 14:48:38 +0300

    Stop-Agent-SO

--- Assets/Scripts/Characters/StateMachine/Actions/StopAgentSO.cs
+++ Assets/Scripts/Characters/StateMachine/Actions/StopAgentSO.cs
@@ -4,6 +4,23 @@ using UOP1.StateMachine;
 using UOP1.StateMachine.ScriptableObjects;
 
 [CreateAssetMenu(fileName = "StopAgent", menuName = "State Machines/Actions/Stop NavMesh Agent")]
+
+public class StopAgentSO : StateActionSO
+{
+	protected override StateAction CreateAction() => new StopAgent();
+}
+
+public class StopAgent : StateAction
+{
+	private NavMeshAgent _agent;
+	private bool _agentDefined;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_agent = stateMachine.gameObject.GetComponent<NavMeshAgent>();
+		_agentDefined = _agent != null;
+	}
+}
 /*public class StopAgentSO : StateActionSO
 {
 	protected override StateAction CreateAction() => new StopAgent();
