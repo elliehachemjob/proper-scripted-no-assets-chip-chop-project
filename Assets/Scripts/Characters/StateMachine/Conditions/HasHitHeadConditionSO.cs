c249c43d6c2e8dfc5e9327ad52c2ee96e62c9593
ellie <elliehachemjob@gmail.com>
2025-07-07 11:45:28 +0300

    Has-Hit-Head-Condition-SO

--- Assets/Scripts/Characters/StateMachine/Conditions/HasHitHeadConditionSO.cs
+++ Assets/Scripts/Characters/StateMachine/Conditions/HasHitHeadConditionSO.cs
@@ -7,8 +7,17 @@ public class HasHitHeadConditionSO : StateConditionSO<HasHitHeadCondition> { }
 
 public class HasHitHeadCondition : Condition
 {
-	//Component references
-	private Protagonist _protagonistScript;
+
+	private CharacterController _characterController;
+	private Transform _transform;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_transform = stateMachine.GetComponent<Transform>();
+		_protagonistScript = stateMachine.GetComponent<Protagonist>();
+		_characterController = stateMachine.GetComponent<CharacterController>();
+	}
+	/*private Protagonist _protagonistScript;
 	private CharacterController _characterController;
 	private Transform _transform;
 
@@ -34,5 +43,33 @@ public class HasHitHeadCondition : Condition
 		}
 
 		return false;
+	}*/
+	//Component references
+	/*private Protagonist _protagonistScript;
+	private CharacterController _characterController;
+	private Transform _transform;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_transform = stateMachine.GetComponent<Transform>();
+		_protagonistScript = stateMachine.GetComponent<Protagonist>();
+		_characterController = stateMachine.GetComponent<CharacterController>();
 	}
+
+	protected override bool Statement()
+	{
+		bool isMovingUpwards = _protagonistScript.movementVector.y > 0f;
+		if (isMovingUpwards)
+		{
+			if(_characterController.collisionFlags == CollisionFlags.Above)
+			{
+				_protagonistScript.jumpInput = false;
+				_protagonistScript.movementVector.y = 0f;
+
+				return true;
+			}
+		}
+
+		return false;
+	}*/
 }
