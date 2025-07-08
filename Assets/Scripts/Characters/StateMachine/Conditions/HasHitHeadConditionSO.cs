25f853d89be4ad7d5f023de3c66418e8c32e4167
ellie <elliehachemjob@gmail.com>
2025-07-08 18:57:56 +0300

    Has-hit-head-condition-so

--- Assets/Scripts/Characters/StateMachine/Conditions/HasHitHeadConditionSO.cs
+++ Assets/Scripts/Characters/StateMachine/Conditions/HasHitHeadConditionSO.cs
@@ -17,6 +17,23 @@ public class HasHitHeadCondition : Condition
 		_protagonistScript = stateMachine.GetComponent<Protagonist>();
 		_characterController = stateMachine.GetComponent<CharacterController>();
 	}
+
+	protected override bool Statement()
+	{
+		bool isMovingUpwards = _protagonistScript.movementVector.y > 0f;
+		if (isMovingUpwards)
+		{
+			if(_characterController.collisionFlags == CollisionFlags.Above)
+			{
+				_protagonistScript.jumpInput = false;
+				_protagonistScript.movementVector.y = 0f;
+
+				return true;
+			}
+		}
+
+		return false;
+	}*/
 	/*private Protagonist _protagonistScript;
 	private CharacterController _characterController;
 	private Transform _transform;
