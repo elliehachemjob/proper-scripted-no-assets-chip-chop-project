e159e6073fed7a34f1c809ed1f9046c350a6eb3d
ellie <elliehachemjob@gmail.com>
2025-07-14 11:53:56 +0300

    Dialogue-Behave

--- Assets/Scripts/Cutscenes/DialogueControlTrack/DialogueBehaviour.cs
+++ Assets/Scripts/Cutscenes/DialogueControlTrack/DialogueBehaviour.cs
@@ -15,6 +15,33 @@ public class DialogueBehaviour : PlayableBehaviour
 	[HideInInspector] public VoidEventChannelSO PauseTimelineEvent;
 
 	private bool _dialoguePlayed;
+	/// <summary>
+	/// Displays a line of dialogue on screen by interfacing with the <c>CutsceneManager</c>. 
+	/// </summary>
+	public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+	{
+		if (_dialoguePlayed)
+			return;
+
+		if (Application.isPlaying)  //TODO: Find a way to "play" dialogue lines even when scrubbing the Timeline not in Play Mode
+		{
+			// Need to ask the CutsceneManager if the cutscene is playing, since the graph is not actually stopped/paused: it's just going at speed 0.
+			if (playable.GetGraph().IsPlaying())
+			//&& cutsceneManager.IsCutscenePlaying) Need to find an alternative to this 
+			{
+				if (_dialogueLine != null && _actor != null)
+				{
+					if (PlayDialogueEvent != null)
+						PlayDialogueEvent.RaiseEvent(_dialogueLine, _actor);
+					_dialoguePlayed = true;s
+				}
+				else
+				{
+					Debug.LogWarning("This clip contains no DialogueLine");
+				}
+			}
+		}
+	}
 
 	/*[SerializeField] private LocalizedString _dialogueLine = default;
 	[SerializeField] private ActorSO _actor = default;
