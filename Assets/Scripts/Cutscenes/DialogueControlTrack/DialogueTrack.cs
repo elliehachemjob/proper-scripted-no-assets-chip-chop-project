d906d1bbfb15b0b9b1cb6c94e91870181648fc2c
ellie <elliehachemjob@gmail.com>
2025-07-18 15:23:11 +0300

    Dialogue-Tracks

--- Assets/Scripts/Cutscenes/DialogueControlTrack/DialogueTrack.cs
+++ Assets/Scripts/Cutscenes/DialogueControlTrack/DialogueTrack.cs
@@ -9,6 +9,17 @@ public class DialogueTrack : PlayableTrack
 	[SerializeField] public DialogueLineChannelSO PlayDialogueEvent;
 	[SerializeField] public VoidEventChannelSO PauseTimelineEvent;
 	
+		public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+	{
+		foreach (TimelineClip clip in GetClips())
+		{
+			DialogueClip dialogueControlClip = clip.asset as DialogueClip;
+			dialogueControlClip.PauseTimelineEvent = PauseTimelineEvent;
+			dialogueControlClip.PlayDialogueEvent = PlayDialogueEvent;
+		}
+
+		return base.CreateTrackMixer(graph, go, inputCount);
+	}
 	/*[SerializeField] public DialogueLineChannelSO PlayDialogueEvent;
 	[SerializeField] public VoidEventChannelSO PauseTimelineEvent;
 	
