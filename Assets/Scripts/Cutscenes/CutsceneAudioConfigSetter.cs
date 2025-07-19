78b16b7ec39f5e93067126182ae030a0222071f8
ellie <elliehachemjob@gmail.com>
2025-07-19 21:50:47 +0300

    Cut-scene-audio-config-setter

--- Assets/Scripts/Cutscenes/CutsceneAudioConfigSetter.cs
+++ Assets/Scripts/Cutscenes/CutsceneAudioConfigSetter.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class CutsceneAudioConfigSetter : MonoBehaviour
 {
-	[SerializeField] private AudioConfigurationSO _audioConfig = default;
+				DialogueClip dialogueControlClip = clip.asset as DialogueClip;
+			dialogueControlClip.PauseTimelineEvent = PauseTimelineEvent;
+			dialogueControlClip.PlayDialogueEvent = PlayDialogueEvent;
+	/*[SerializeField] private AudioConfigurationSO _audioConfig = default;
 	[SerializeField] private VoidEventChannelSO onCutsceneStart = default;
 
 	private void OnEnable()
@@ -18,5 +21,5 @@ public class CutsceneAudioConfigSetter : MonoBehaviour
 	private void SetVolume()
 	{
 		GetComponent<AudioSource>().volume = _audioConfig.Volume;
-	}
+	}*/
 }
