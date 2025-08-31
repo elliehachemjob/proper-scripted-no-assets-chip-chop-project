f5d497a246e2b04ac584e9631860123a46dda773
ellie <elliehachemjob@gmail.com>
2025-08-31 15:13:01 +0300

    Cut-scene-loader

--- Assets/Scripts/Cutscenes/CutsceneSceneLoader.cs
+++ Assets/Scripts/Cutscenes/CutsceneSceneLoader.cs
@@ -6,7 +6,11 @@ public class CutsceneSceneLoader : MonoBehaviour
 
 	[Header("Broadcasting on")]
 	[SerializeField] private LoadEventChannelSO _sceneLoadChannel = default;
-
+	//Used to load a location or menu from a cutscene
+	public void LoadScene()
+	{
+		_sceneLoadChannel.RaiseEvent(_sceneToLoad, false, true);
+	}
 	/*[SerializeField] private GameSceneSO _sceneToLoad = default;
 
 	[Header("Broadcasting on")]
