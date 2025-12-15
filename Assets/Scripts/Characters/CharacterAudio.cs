64be2c3a879f90e487ba706bbae04ede6b751fdb
ellie <elliehachemjob@gmail.com>
2025-12-15 09:50:52 +0200

    Character-Audio-2

--- Assets/Scripts/Characters/CharacterAudio.cs
+++ Assets/Scripts/Characters/CharacterAudio.cs
@@ -13,4 +13,13 @@ public class CharacterAudio : MonoBehaviour
 		if (_gameState.CurrentGameState != GameState.Cutscene)
 			_sfxEventChannel.RaisePlayEvent(audioCue, audioConfiguration, positionInSpace);
 	}*/
+	 [SerializeField] protected AudioCueEventChannelSO _sfxEventChannel = default;
+	[SerializeField] protected AudioConfigurationSO _audioConfig = default;
+	[SerializeField] protected GameStateSO _gameState = default;
+	
+	protected void PlayAudio(AudioCueSO audioCue, AudioConfigurationSO audioConfiguration, Vector3 positionInSpace = default)
+	{
+		if (_gameState.CurrentGameState != GameState.Cutscene)
+			_sfxEventChannel.RaisePlayEvent(audioCue, audioConfiguration, positionInSpace);
+	}
 }
