6161d4cd97b7a26ca28cfde6453e1f472a4c1e6f
ellie <elliehachemjob@gmail.com>
2026-01-12 07:32:26 +0200

    Critter-audio

--- Assets/Scripts/Characters/CritterAudio.cs
+++ Assets/Scripts/Characters/CritterAudio.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CritterAudio : CharacterAudio
 {
+	
 	[SerializeField] private AudioCueSO idleSound, moveSound, attackSound, gettingHitSound, deathSound;
 
 	public void PlayIdleSound() => PlayAudio(idleSound, _audioConfig, transform.position);
@@ -10,5 +11,13 @@ public class CritterAudio : CharacterAudio
 	public void PlayAttackSound() => PlayAudio(attackSound, _audioConfig, transform.position);
 	public void PlayGettingHitSound() => PlayAudio(gettingHitSound, _audioConfig, transform.position);
 	public void PlayDeathSound() => PlayAudio(deathSound, _audioConfig, transform.position);
+	/* [SerializeField] private AudioCueSO idleSound, moveSound, attackSound, gettingHitSound, deathSound;
 
+	public void PlayIdleSound() => PlayAudio(idleSound, _audioConfig, transform.position);
+	//The move sound will not be called for the plant critter
+	public void PlayMoveSound() => PlayAudio(moveSound, _audioConfig, transform.position);
+	public void PlayAttackSound() => PlayAudio(attackSound, _audioConfig, transform.position);
+	public void PlayGettingHitSound() => PlayAudio(gettingHitSound, _audioConfig, transform.position);
+	public void PlayDeathSound() => PlayAudio(deathSound, _audioConfig, transform.position);
+*/
 }
