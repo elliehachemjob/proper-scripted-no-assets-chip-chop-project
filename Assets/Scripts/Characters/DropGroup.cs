96575ddf30a81224f8ef5542d9b4cc4e2db71a28
ellie <elliehachemjob@gmail.com>
2026-02-02 11:07:53 +0200

    Drop-Group

--- Assets/Scripts/Characters/DropGroup.cs
+++ Assets/Scripts/Characters/DropGroup.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 [Serializable]
 public class DropGroup
 {
+	
 	[SerializeField] List<DropItem> _drops;
 	[SerializeField] float _dropRate;
 
 	public List<DropItem> Drops => _drops;
 	public float DropRate => _dropRate;
+
+	/*erializeField] List<DropItem> _drops;
+	[SerializeField] float _dropRate;
+
+	public List<DropItem> Drops => _drops;
+	public float DropRate => _dropRate;
+
+*/
+
 }
