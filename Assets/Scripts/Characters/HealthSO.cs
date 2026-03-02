7b868fd8a696bb9a3717d6f783d37a03ddf68107
Ellie <elliehachemjob@gmail.com>
2026-03-02 11:23:53 +0200

    Health-SO-part2

--- Assets/Scripts/Characters/HealthSO.cs
+++ Assets/Scripts/Characters/HealthSO.cs
@@ -7,10 +7,28 @@
 [CreateAssetMenu(fileName = "PlayersHealth", menuName = "EntityConfig/Player's Health")]
 public class HealthSO : ScriptableObject
 {
-	
+
 	[Tooltip("The initial health")]
 	[SerializeField][ReadOnly] private int _maxHealth;
 	[SerializeField][ReadOnly] private int _currentHealth;
+	
+	public int MaxHealth => _maxHealth;
+	public int CurrentHealth => _currentHealth;
+
+	public void SetMaxHealth(int newValue)
+	{
+		_maxHealth = newValue;
+	}
+
+	public void SetCurrentHealth(int newValue)
+	{
+		_currentHealth = newValue;
+	}
+	
+	public void InflictDamage(int DamageValue)
+	{
+		_currentHealth -= DamageValue;
+	}
 	/* [Tooltip("The initial health")]
 	[SerializeField][ReadOnly] private int _maxHealth;
 	[SerializeField][ReadOnly] private int _currentHealth;
